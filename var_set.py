fac7cabccd1d3b5fd444a826ae9a658d5e2d672e
wuqing234 <33693575+wuqing234@users.noreply.github.com>
2018-09-10 09:52:13 +0800

    Update var_set.py

--- var_set.py
+++ var_set.py
@@ -5,20 +5,20 @@
 path = '/home/pi/live'
 #本文件的路径，请修改
 
-roomid = '16703'
+roomid = '205175'
 #房间id（真实id，不一定是网址里的那个数）
 
-cookie = ''
+cookie = 'finger=edc6ecda; buvid3=20A59A66-E31C-4516-9DA5-BF5332B9A3DC16095infoc; sid=aetvmslp; DedeUserID=287990140; DedeUserID__ckMd5=c828c660f915a2c8; SESSDATA=0f497c2f%2C1539135531%2Cb468664a; bili_jct=9ec17bd356ca26dd06b0ff6331ffb292; LIVE_BUVID=c2d2c7b75eac7fda9b1f0e6497816aa1; _dfcaptcha=e02671b87f963cb1d37e761021a17027; fts=1536543547; UM_distinctid=165c121df41158-0e8495d3cd41d4-9393265-144000-165c121df435b9; bp_t_offset_287990140=161703268132895259; LIVE_BUVID__ckMd5=472280cec355fd6c'
 #发送弹幕用的cookie
-csrf_token = ''
+csrf_token = '9ec17bd356ca26dd06b0ff6331ffb292'
 #发送弹幕用的csrf_token
 
 download_api_url = 'https://qq.papapoi.com/163music/'
 #获取音乐链接的api网址，服务器性能有限，尽量请换成自己的，php文件在php文件夹
 
-rtmp = 'rtmp://xxxxx.live-send.acg.tv/live-xxxxx/'
+rtmp = 'rtmp://txy.live-send.acg.tv/live-txy/'
 #直播给的两个码，填在这里
-live_code = ''
+live_code = '?streamname=live_18085741_9906629&key=f61ebeb4e725ffed876575c3ebcd06d9'
 
 free_space=15360
 #允许download/default_mp3文件夹占用空间大小，超过时自动按时间顺序删除视频/音乐，单位：MiB
