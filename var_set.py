5529039791e587401eb973d1e6150bb79d48395d
wuqing234 <33693575+wuqing234@users.noreply.github.com>
2018-09-18 21:42:38 +0800

    Update var_set.py

--- var_set.py
+++ var_set.py
@@ -8,7 +8,7 @@ path = '/home/pi/share'
 roomid = '205175'
 #房间id（真实id，不一定是网址里的那个数）
 
-cookie = 'buvid3=A33D94EB-1970-4F70-810A-A5A88F225B0E16065infoc; finger=edc6ecda; LIVE_BUVID=2f650f6c76ac2829f0f391a4f2eb1e71; sid=8525ub9l; DedeUserID=287990140; DedeUserID__ckMd5=c828c660f915a2c8; SESSDATA=0f497c2f%2C1539846725%2Cb24a836c; bili_jct=0e764ad33be2bc01dda32f92dc0bbb52; _dfcaptcha=1968d834138b69ef499d1811baf410cc; im_notify_type_287990140=0; bp_t_offset_287990140=164931850883542448; fts=1537254904; UM_distinctid=165eb884f6b37-0fced4333db1f-9393265-144000-165eb884f6c409; LIVE_BUVID__ckMd5=c6498ab0b5d2fe94
+cookie = 'im_notify_type_287990140=0; buvid3=A33D94EB-1970-4F70-810A-A5A88F225B0E16065infoc; LIVE_BUVID=2f650f6c76ac2829f0f391a4f2eb1e71; sid=8525ub9l; DedeUserID=287990140; DedeUserID__ckMd5=c828c660f915a2c8; SESSDATA=0f497c2f%2C1539846725%2Cb24a836c; bili_jct=0e764ad33be2bc01dda32f92dc0bbb52; finger=edc6ecda; bp_t_offset_287990140=165039706103486238; fts=1537254904; UM_distinctid=165eb884f6b37-0fced4333db1f-9393265-144000-165eb884f6c409; LIVE_BUVID__ckMd5=c6498ab0b5d2fe94; GIFT_BLOCK_COOKIE=GIFT_BLOCK_COOKIE; GIFT_BLOCK_COOKIE=GIFT_BLOCK_COOKIE; _dfcaptcha=54c5eaf5f2378a4e3aa0f75d5887cc29'
 #发送弹幕用的cookie
 csrf_token = '0e764ad33be2bc01dda32f92dc0bbb52'
 #发送弹幕用的csrf_token
